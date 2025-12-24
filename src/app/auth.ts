52ed49e603722cde8dc41ebee8ea59d263db2298
Dnyanunik <dnyaneshwarn727@gmail.com>
2025-12-24 21:15:03 +0530

    Initial commit - Angular with Node.js and SSR

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class AuthService {
 
- private API_URL = 'https://my-polly-app.onrender.com/api/auth';
+ private API_URL = 'https://my-polly-app.onrender.com';
   constructor(private http: HttpClient) { }
 
 register(data: any) {
